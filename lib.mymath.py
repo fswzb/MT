6c3103c350e2bb057684ce05f2a44f495c27d03b
sunxdc15 <sxdemail@163.com>
2017-07-05 23:23:32 +0800

    四舍五入函数修改

--- lib.mymath.py
+++ lib.mymath.py
@@ -1,15 +1,10 @@
 # round函数四舍五入
-def rod(origin,n):
-    rd = round(origin,n)
-    ird = int(rd*10**(n+1))
-    yu = 5
-    origin = round(origin,n+1)
-    diff = int(origin*10**(n+1)) - ird
-    diff = diff - yu
-    if  diff >= 0:
-        rd = rd + 1./10**n
-    return round(rd,n)
-
+def rod(x,n):
+    #first omit n+1
+    xn1=int(x*10**(n+1))
+    #then add 1 at the end to avoid the float precise
+    xn1=xn1+0.1
+    return round(xn1/10**(n+1),n)
 def is_number(s):
     try:
         float(s)
@@ -17,4 +12,7 @@ def is_number(s):
     except ValueError:
         pass
     return False
-#rod(0.45,1)
+#print round(299.455,2)
+#print rod(299.455,2)
+#print round(39.955,2)
+#print rod(39.955,2)
